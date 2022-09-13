cba9a5ccbcc94b6db26d9906904443fae09b8399
Linus Ericsson <linus.ericsson@ed.ac.uk>
2022-09-13 13:58:10 +0100

    Update download_and_prep.sh

--- download_and_prep.sh
+++ download_and_prep.sh
@@ -1,4 +1,4 @@
 wget http://csr.bu.edu/ftp/visda17/clf/train.tar && tar xvf train.tar && rm train.tar
 wget http://csr.bu.edu/ftp/visda17/clf/validation.tar && tar xvf validation.tar && rm validation.tar
-wget http://csr.bu.edu/ftp/visda17/clf/test.tar tar xvf test.tar && rm test.tar
+wget http://csr.bu.edu/ftp/visda17/clf/test.tar && tar xvf test.tar && rm test.tar
 wget https://raw.githubusercontent.com/VisionLearningGroup/taskcv-2017-public/master/classification/data/image_list.txt
