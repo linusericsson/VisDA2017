50dae9ec34592857183db595a3a3740ca9a007e1
Linus Ericsson <linus.ericsson@ed.ac.uk>
2022-10-12 13:52:15 +0100

    Update download_and_prep.sh

--- download_and_prep.sh
+++ download_and_prep.sh
@@ -1,4 +1,9 @@
+echo "preparing classification track"
 wget http://csr.bu.edu/ftp/visda17/clf/train.tar && tar xvf train.tar && rm train.tar
 wget http://csr.bu.edu/ftp/visda17/clf/validation.tar && tar xvf validation.tar && rm validation.tar
 wget http://csr.bu.edu/ftp/visda17/clf/test.tar && tar xvf test.tar && rm test.tar
 wget https://raw.githubusercontent.com/VisionLearningGroup/taskcv-2017-public/master/classification/data/image_list.txt
+
+echo "preparing segmentation track - test only"
+wget http://csr.bu.edu/ftp/visda17/seg/test.zip
+tar xvf test.zip
